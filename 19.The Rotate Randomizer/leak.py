e910884d4b98a4a4ecf9914827bea12f200545ff
Xelenonz <xelenonz@gmail.com>
2015-02-16 13:33:55 +0700

    Add Chall #19 The Rotate Randomizer

---
+++ 19.The Rotate Randomizer/leak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding: rot13
+vzcbeg enaqbz
+
+e = enaqbz.Enaqbz()
+cnffjbeq = "You cannot find me :D"
+e.frrq(cnffjbeq)
+
+A = vag(enj_vachg('[-] Enter Generate Count : '))
+
+sbe v va enatr(A):
+    cevag "[+] @%d Generated : %d " % (v+1, e.trgenaqovgf(32))
+
+
+'''
+
+Example Output:
+[-] Enter Generate Count : 5
+[+] @1 Generated : 2920747024
+[+] @2 Generated : 2838095342
+[+] @3 Generated : 2395227559
+[+] @4 Generated : 343641803
+[+] @5 Generated : 1311103408   
+
+'''
+
